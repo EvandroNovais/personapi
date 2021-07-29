d36615b8ea1f22367415fa74d60a576924fec228
EvandroNovais <evandro-novais@outlook.com>
2021-07-29 17:34:38 -0300

    include: PersonService.

---
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.personapi.service;
+
+import one.digitalinnovation.personapi.dto.MessagePersonDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessagePersonDTO createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessagePersonDTO
+                .builder()
+                .message("Created person with Id " + savedPerson.getId())
+                .build();
+    }
+}
